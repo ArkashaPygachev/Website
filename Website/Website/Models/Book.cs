c57b9029fc7b1df33743aa843491d7fb80416975
ArkashaPygachev <93211239@mail.ru>
2020-02-19 21:57:06 +0300

    Siple book store

---
+++ Website/Website/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Models {
+    public class Book {
+        public int BookId { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ Website/Website/Models/Purchase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Models {
+    public class Purchase {
+        public int PurchaseId { get; set; }
+        public string Person { get; set; }
+        public string Addres { get; set; }
+        public int BookId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
